6a6c9e9f3730a32a04fdebe796a7b0c327092b69
Manikaran <kathuriamanikaran.com>
2017-08-04 22:42:43 +0530

    Fifth commit- Security class added

---
+++ src/node/Security.java
@@ -0,0 +1,160 @@
+package node;
+
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class Security {
+	
+	private PrivateKey privateKey;
+	private PublicKey publicKey;
+	
+	public Security() {
+		privateKey = null;
+		publicKey = null;
+	}
+	
+	public byte[] getHash(String msg) {
+		byte[] hash = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA1");
+			hash = md.digest(msg.getBytes());
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return hash;
+	}
+	
+	private void generateKeyPair() {
+		try {
+			KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+			KeyPair kp = gen.generateKeyPair();
+			privateKey = kp.getPrivate();
+			publicKey = kp.getPublic();
+		
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public String getPrivateKey() {
+		if(privateKey == null) {
+			generateKeyPair();
+		}
+		return privateKey.toString();
+	}
+	
+	public String getPublicKey() {
+		if(publicKey == null) {
+			generateKeyPair();
+		}
+		return publicKey.toString();
+	}
+	
+	private byte[] encrypt (byte[] plainText) {
+		byte[] cipherTextBytes = null;
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+			cipherTextBytes = cipher.doFinal(plainText);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cipherTextBytes;
+	}
+	
+	private byte[] decrypt(byte[] cipherText) {
+		byte[] plainTextBytes = null;
+		try {
+			Cipher cipher = Cipher.getInstance("RSA");
+			cipher.init(Cipher.DECRYPT_MODE, publicKey);
+			plainTextBytes = cipher.doFinal(cipherText);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalBlockSizeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return plainTextBytes;
+		
+	}
+	
+	public byte[] createDigest(String msg) {
+		byte[] hash = getHash(msg);
+		byte[] encryptedHash = encrypt(hash);
+		return encryptedHash;
+	}
+	
+	public boolean verifySignature(String msg, byte[] signature) {
+		byte[] hash = decrypt(signature);
+		byte[] msgHash = getHash(msg);
+		if(bytesToString(hash).equals(bytesToString(msgHash)))
+			return true;
+		return false;
+	}
+	
+	private String bytesToString(byte[] msg) {
+		StringBuffer buffer = new StringBuffer("");
+		for(int i=0;i<msg.length;i++) {
+			buffer.append(Integer.toString((msg[i]&0xff),16));
+		}
+		
+		return buffer.toString();
+	}
+	
+	public static void main(String[] args) {
+		Security testObject = new Security();
+		String msg = "Hello, this is the trial msg";
+		byte[] hash = testObject.getHash(msg);
+		String msgHash = testObject.bytesToString(hash);
+		testObject.generateKeyPair();
+		System.out.println("This is the message hash : " + msgHash);
+		byte[] cipherText = testObject.encrypt(hash);
+		String encryptedHash = testObject.bytesToString(cipherText);
+		System.out.println("This is the encrypted hash : " + encryptedHash);
+		String decryptedHash = testObject.bytesToString(testObject.decrypt(cipherText));
+		System.out.println("This is the decrypted hash : " + decryptedHash);
+		if(testObject.verifySignature(msg, cipherText)) {
+			System.out.println("Authenticated Message");
+		}
+		else {
+			System.out.println("Message not authenticated");
+		}
+	}
+}
